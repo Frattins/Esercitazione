26d9b64a031f2255284757866dd711187048fa1a
Frattins <davide.frattini1@gmail.com>
2024-06-17 15:24:45 +0200

    #esercizio backend S1/D1

--- Esercitazione/Atleta.cs
+++ Esercitazione/Atleta.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,23 @@ namespace Esercitazione
 {
     internal class Atleta
     {
+        string nome;
+        string cognome;
+        string sport;
+
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public void descrizione()
+        {
+            Console.WriteLine(nome);
+            Console.WriteLine(cognome);
+            Console.WriteLine(sport);
+            Console.WriteLine("Mi chiamo" + nome + " " + cognome + " e pratico " + sport););
+        }
+
     }
 }
